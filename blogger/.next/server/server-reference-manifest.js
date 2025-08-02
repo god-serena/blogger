2332f25d13ffa80b0f8863a222ca8791d38ff9d6
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-08-02 11:24:08 +0800

    Copy over next.js to this repo

---
+++ blogger/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"xJ9rfrYUAvWhQEEinu9i8fRVhGmTgiQLRfJzsiTzf1Q=\"\n}"
---
+++ blogger/.next/static/chunks/src_app_blogs_page_tsx_ecce0985._.js
@@ -0,0 +1,10 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_quill_quill_75aac5b4.js",
+  "static/chunks/node_modules_efafff47._.js",
+  "static/chunks/src_53e47090._.js",
+  "static/chunks/_81192a09._.css"
+],
+    source: "dynamic"
+});
