bd5119855b01dee01a9fc322c8bb0cc75145ff38
Ar-jay Dayanan <acdayanan.bposeats@gmail.com>
2025-08-18 01:29:54 +0800

    Instantiate settings object to obtain env vars

--- blogger_api/src/settings.py
+++ blogger_api/src/settings.py
@@ -1,1 +1,10 @@
 from pydantic_settings import BaseSettings
+
+class Settings(BaseSettings):
+    SQLALCHEMY_DATABASE_URL: str = "postgresql://user:password@blogger_db:5432/blogger_db"
+    DEBUG: bool = False
+    ACCESS_TOKEN_LIFETIME: int = 1
+    REFRESH_TOKEN_LIFETIME: int = 7
+    SECRET_KEY: str = "blogger_api_dev"
+
+settings = Settings()
